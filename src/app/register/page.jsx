9b80979b201dac500664109c106ea33e00247e94
AlvDa780 <danyalves.r11@gmail.com>
2024-03-13 16:24:28 +0100

    auth

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,6 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import { AtSign, LockKeyhole, User, MoveLeft } from "lucide-react";
+import {
+  AtSign,
+  LockKeyhole,
+  User,
+  MoveLeft,
+  TriangleAlert,
+} from "lucide-react";
 import Link from "next/link";
 import { supabase } from "../../utils/supabase";
 import { useForm } from "react-hook-form";
@@ -11,7 +17,11 @@ export default function Index() {
   const [username, setUsername] = useState("");
   const [confPassword, setConfPassword] = useState("");
 
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => console.log(data);
 
@@ -28,7 +38,7 @@ export default function Index() {
   // };
 
   return (
-    <div className="mx-auto h-screen container flex flex-col space-y-5 justify-center items-center pt-8 ">
+    <div className="mx-auto h-screen container flex flex-col space-y-3 justify-center items-center ">
       <div className="flex justify-start w-80">
         <Link className="flex items-center " href="/home">
           <MoveLeft className="pr-2" size={32} />
@@ -37,17 +47,26 @@ export default function Index() {
       </div>
       <h1 className="flex justify-center text-3xl">Register</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="w-full  flex flex-col space-y-5 items-center justify-center">
-          <label className="w-80 flex items-center  border border-white pl-2 ">
+        <div className="flex flex-col space-y-3 items-center justify-center">
+          <label className="w-80 flex items-center border border-white pl-2 ">
             <User size={18} />
             <input
               className=" ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-              {...register("Username", { required: true, maxLength: 20 })}
+              {...register("username", { required: true, maxLength: 20 })}
               value={username}
               placeholder="Enter your Username"
               onChange={(e) => setUsername(e.target.value)}
             />
           </label>
+
+          <div className=" w-full justify-center flex ">
+            {errors?.username?.type === "required" && (
+              <p className=" flex items-center justify-center text-red-400">
+                <TriangleAlert size={18} />
+                This field is required
+              </p>
+            )}
+          </div>
 
           <label className="w-80 flex items-center  border border-white pl-2 ">
             <AtSign size={18} />
@@ -60,19 +79,40 @@ export default function Index() {
             />
           </label>
 
+          <div className=" w-full justify-center flex ">
+            {errors?.username?.type === "required" && (
+              <p className=" flex items-center justify-center text-red-400">
+                <TriangleAlert size={18} />
+                This field is required
+              </p>
+            )}
+          </div>
+
           <label className="w-80 flex items-center  border border-white pl-2 ">
             <LockKeyhole size={18} />
+
             <input
               className="ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
               type="password"
+              {...register("Password")}
               value={password}
               placeholder="Enter your Password"
               onChange={(e) => setPassword(e.target.value)}
             />
           </label>
 
-          <label className="w-80 flex items-center  border border-white pl-2 ">
+          <div className=" w-full justify-center flex ">
+            {errors?.username?.type === "required" && (
+              <p className=" flex items-center justify-center text-red-400">
+                <TriangleAlert size={18} />
+                This field is required
+              </p>
+            )}
+          </div>
+
+          <label className="w-80 flex items-center border border-white pl-2 ">
             <LockKeyhole size={18} />
+
             <input
               className="ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
               type="password"
@@ -81,6 +121,15 @@ export default function Index() {
               onChange={(e) => setConfPassword(e.target.value)}
             />
           </label>
+
+          <div className=" w-full justify-center flex ">
+            {errors?.username?.type === "required" && (
+              <p className=" flex items-center justify-center text-red-400">
+                <TriangleAlert size={18} />
+                This field is required
+              </p>
+            )}
+          </div>
 
           <div className="flex justify-center">
             <button
