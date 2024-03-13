d2b3d4dfa06b5512a9870c565febd6fbc7ae3d30
AlvDa780 <danyalves.r11@gmail.com>
2024-03-13 15:07:47 +0100

    auth

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { AtSign, LockKeyhole, User, MoveLeft } from "lucide-react";
 import Link from "next/link";
 import { supabase } from "../../utils/supabase";
-
+import { useForm } from "react-hook-form";
 
 export default function Index() {
   const [email, setEmail] = useState("");
@@ -11,19 +11,21 @@ export default function Index() {
   const [username, setUsername] = useState("");
   const [confPassword, setConfPassword] = useState("");
 
+  const { register, handleSubmit } = useForm();
 
+  const onSubmit = (data) => console.log(data);
 
-  const submit = async () => {
-    const user = {
-      email: email,
-      password: password,
-      username: username,
-    };
-    console.log("🚀 ~ submit ~ user:", user)
+  // const submit = async () => {
 
-    await supabase.from("users").insert(user);
-    
-  };
+  //   const user =  {
+  //     email: email,
+  //     password: password,
+  //     username: username,
+  //   };
+  //   console.log("🚀 ~ submit ~ user:", user);
+
+  //   // await supabase.from("users").insert(user);
+  // };
 
   return (
     <div className="mx-auto h-screen container flex flex-col space-y-5 justify-center items-center pt-8 ">
@@ -34,63 +36,62 @@ export default function Index() {
         </Link>
       </div>
       <h1 className="flex justify-center text-3xl">Register</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="w-full  flex flex-col space-y-5 items-center justify-center">
+          <label className="w-80 flex items-center  border border-white pl-2 ">
+            <User size={18} />
+            <input
+              className=" ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              {...register("Username", { required: true, maxLength: 20 })}
+              value={username}
+              placeholder="Enter your Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
 
-      <div className="w-full  flex flex-col space-y-5 items-center justify-center">
-        <div className="w-80 flex items-center  border border-white pl-2 ">
-          <User size={18} />
-          <input
-            className=" ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-            type="Email"
-            value={username}
-            placeholder="Enter your Username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <label className="w-80 flex items-center  border border-white pl-2 ">
+            <AtSign size={18} />
+            <input
+              className=" ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              {...register("Email")}
+              value={email}
+              placeholder="Enter your Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+
+          <label className="w-80 flex items-center  border border-white pl-2 ">
+            <LockKeyhole size={18} />
+            <input
+              className="ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              value={password}
+              placeholder="Enter your Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+
+          <label className="w-80 flex items-center  border border-white pl-2 ">
+            <LockKeyhole size={18} />
+            <input
+              className="ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              value={confPassword}
+              placeholder="Confirm your Password"
+              onChange={(e) => setConfPassword(e.target.value)}
+            />
+          </label>
+
+          <div className="flex justify-center">
+            <button
+              className="w-80 bg-green-400 pl-1 pr-1 pt-1 pb-1 "
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
-
-        <div className="w-80 flex items-center  border border-white pl-2 ">
-          <AtSign size={18} />
-          <input
-            className=" ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-            type="Email"
-            value={email}
-            placeholder="Enter your Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className="w-80 flex items-center  border border-white pl-2 ">
-          <LockKeyhole size={18} />
-          <input
-            className="ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-            type="Password"
-            value={password}
-            placeholder="Enter your Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className="w-80 flex items-center  border border-white pl-2 ">
-          <LockKeyhole size={18} />
-          <input
-            className="ml-1 pl-2 pt-1 pb-1 bg-black shadow appearance-none w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-            type="password"
-            value={confPassword}
-            placeholder="Confirm your Password"
-            onChange={(e) => setConfPassword(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="flex justify-center">
-        <button
-          className="w-80 bg-green-400 pl-1 pr-1 pt-1 pb-1 "
-          type="submit"
-    onClick={submit}
-        >
-          Sign Up
-        </button>
-      </div>
-
+      </form>
     </div>
   );
 }
